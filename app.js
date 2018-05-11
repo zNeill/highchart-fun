ddac462e63b55759560390e327bd0dc4f603f876
Neill Lewis <nlewis@tritansoftware.com>
2018-05-11 12:03:03 -0400

    refactoring to avoid globals

--- app.js
+++ app.js
@@ -79,55 +79,57 @@ function arrayFromKey(obj,key)
 var barChartEvents = organizeByKey(srcJSON, 'eventType1', 'eventType2'),
     barChartCategories = arrayFromKey(barChartEvents, 'name'),
     barChartCounts = arrayFromKey(barChartEvents, 'count'),
-    selectedPie = barChartEvents[0],
-    pieEvents = organizeByKey(selectedPie.data, 'eventType2', 'eventType3'),
+    selectedEventType1 = barChartEvents[0],
     colors = Highcharts.getOptions().colors,
     brightness,
-    et2Data = [],
-    et3Data = [];
-
-buildPieData();
+    chartA,
+    chartB,
+    chartCData = organizeByKey(selectedEventType1.data, 'ship', 'severity');
+let pieEvents = organizeByKey(selectedEventType1.data, 'eventType2', 'eventType3');
+console.log(chartCData);
+buildPieChart(pieEvents, "Fire/Explosion");
+buildShipSeverityData();
 
 /////////////////////////////
 //     Chart Updater       //
 /////////////////////////////
 
 function updateSubCharts(category) {
-  console.log('we made it this far with ' + category);
   var ix = barChartCategories.indexOf(category);
-  selectedPie = barChartEvents[ix];
-  pieEvents = organizeByKey(selectedPie.data, 'eventType2', 'eventType3'),
-  buildPieData();
-  chartB.update({
-    title: {
-      text: category
-    },
-    series: [{
-      name: 'Event Type 2',
-      data: et2Data,
-      size: '60%',
-      dataLabels: {
-        formatter: function () {
-          return this.y > 5 ? this.point.name : null;
-        },
-        color: '#ffffff',
-        distance: -30
-      }
-    }, {
-      name: 'Event Type 3',
-      data: et3Data,
-      size: '80%',
-      innerSize: '60%',
-      dataLabels: {
-        formatter: function () {
-          // display only if larger than 1
-          return this.y > 1 ? '<b>' + this.point.name + ':</b> ' +
-            this.y + '%' : null;
-        }
-      },
-      id: 'versions'
-    }],
-  })
+  selectedEventType1 = barChartEvents[ix];
+  pieEvents = organizeByKey(selectedEventType1.data, 'eventType2', 'eventType3');
+  let isUpdate = true;
+  buildPieChart(pieEvents, category, isUpdate);
+  // chartB.update({
+  //   title: {
+  //     text: category
+  //   },
+  //   series: [{
+  //     name: 'Event Type 2',
+  //     data: et2Data,
+  //     size: '60%',
+  //     dataLabels: {
+  //       formatter: function () {
+  //         return this.y > 5 ? this.point.name : null;
+  //       },
+  //       color: '#ffffff',
+  //       distance: -30
+  //     }
+  //   }, {
+  //     name: 'Event Type 3',
+  //     data: et3Data,
+  //     size: '80%',
+  //     innerSize: '60%',
+  //     dataLabels: {
+  //       formatter: function () {
+  //         // display only if larger than 1
+  //         return this.y > 1 ? '<b>' + this.point.name + ':</b> ' +
+  //           this.y + '%' : null;
+  //       }
+  //     },
+  //     id: 'versions'
+  //   }],
+  // })
 }
 
 //////////////////////////
@@ -173,10 +175,10 @@ var chartA = Highcharts.chart('chart-a', {
 ////////////////////////////
 
 
-function buildPieData()
+function buildPieChart(pieEvents, pieTitle, update)
 {
-  et2Data = [];
-  et3Data = [];
+  let et2Data = [];
+  let et3Data = [];
   // Build the data arrays
   for (let i = 0; i < pieEvents.length; i++) {
 
@@ -197,68 +199,162 @@ function buildPieData()
       });
     }
   }
-
-}
-
-var chartB = Highcharts.chart('chart-b', {
-  chart: {
-    type: 'pie'
-  },
-  title: {
-    text: barChartEvents[0].name
-  },
-  yAxis: {
-    title: {
-      text: 'events'
-    }
-  },
-  plotOptions: {
-    pie: {
-      shadow: false,
-      center: ['50%', '50%']
-    }
-  },
-  tooltip: {
-    valueSuffix: '%'
-  },
-  series: [{
-    name: 'Event Type 2',
-    data: et2Data,
-    size: '60%',
-    dataLabels: {
-      formatter: function () {
-        return this.y > 5 ? this.point.name : null;
+  if(!update) { 
+    chartB = Highcharts.chart('chart-b', {
+      chart: {
+        type: 'pie'
       },
-      color: '#ffffff',
-      distance: -30
-    }
-  }, {
-    name: 'Event Type 3',
-    data: et3Data,
-    size: '80%',
-    innerSize: '60%',
-    dataLabels: {
-      formatter: function () {
-        // display only if larger than 1
-        return this.y > 1 ? '<b>' + this.point.name + ':</b> ' +
-          this.y + '%' : null;
-      }
-    },
-    id: 'versions'
-  }],
-  responsive: {
-    rules: [{
-      condition: {
-        maxWidth: 500,
+      title: {
+        text: pieTitle
       },
-      chartOptions: {
-        series: [{
-          id: 'versions',
-          dataLabels: {
-            enabled: false
+      yAxis: {
+        title: {
+          text: 'events'
+        }
+      },
+      plotOptions: {
+        pie: {
+          shadow: false,
+          center: ['50%', '50%']
+        }
+      },
+      tooltip: {
+        valueSuffix: '%'
+      },
+      series: [{
+        name: 'Event Type 2',
+        data: et2Data,
+        size: '60%',
+        dataLabels: {
+          formatter: function () {
+            return this.y > 5 ? this.point.name : null;
+          },
+          color: '#ffffff',
+          distance: -30
+        }
+      }, {
+        name: 'Event Type 3',
+        data: et3Data,
+        size: '80%',
+        innerSize: '60%',
+        dataLabels: {
+          formatter: function () {
+            // display only if larger than 1
+            return this.y > 1 ? '<b>' + this.point.name + ':</b> ' +
+              this.y + '%' : null;
+          }
+        },
+        id: 'versions'
+      }],
+      responsive: {
+        rules: [{
+          condition: {
+            maxWidth: 500,
+          },
+          chartOptions: {
+            series: [{
+              id: 'versions',
+              dataLabels: {
+                enabled: false
+              }
+            }]
           }
         }]
       }
-    }]
+    });
+  } else {
+    
+    chartB.update({
+      title: {
+        text: pieTitle
+      },
+      series: [{
+        name: 'Event Type 2',
+        data: et2Data,
+        size: '60%',
+        dataLabels: {
+          formatter: function () {
+            return this.y > 5 ? this.point.name : null;
+          },
+          color: '#ffffff',
+          distance: -30
+        }
+      }, {
+        name: 'Event Type 3',
+        data: et3Data,
+        size: '80%',
+        innerSize: '60%',
+        dataLabels: {
+          formatter: function () {
+            // display only if larger than 1
+            return this.y > 1 ? '<b>' + this.point.name + ':</b> ' +
+              this.y + '%' : null;
+          }
+        },
+        id: 'versions'
+      }],
+    })
   }
-});
+}
+
+
+//////////////////////////////////
+// Chart C (Ship then Severity) //
+//////////////////////////////////
+function buildShipSeverityData() {
+
+
+  var chartC = Highcharts.chart('chart-c', {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Monthly Average Rainfall'
+    },
+    subtitle: {
+      text: 'Source: WorldClimate.com'
+    },
+    xAxis: {
+      categories: arrayFromKey(chartCData,'name'),
+      crosshair: true
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Rainfall (mm)'
+      }
+    },
+    tooltip: {
+      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+      pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+        '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+      footerFormat: '</table>',
+      shared: true,
+      useHTML: true
+    },
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0
+      }
+    },
+    series: [{
+      name: 'Tokyo',
+      data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+    }, {
+      name: 'New York',
+      data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+    }, {
+      name: 'London',
+      data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+
+    }, {
+      name: 'Berlin',
+      data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+
+    }]
+  });
+}
+
